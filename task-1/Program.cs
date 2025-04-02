2bff2dd2d1a62b8da49679edc97f96b7dff1583d
SanthoshKannanSP <mail2santhoshkannan@gmail.com>
2025-04-02 21:33:11 +0530

    Task-1 factorial program

---
+++ task-1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Numerics;
+
+BigInteger CalculateFactorial(int n)
+{
+    if (n == 0 || n == 1)
+    {
+        return 1;
+    }
+    return n * CalculateFactorial(n - 1);
+}
+
+Console.Write("Enter a number: ");
+if (int.TryParse(Console.ReadLine(), out int number))
+{
+    if (number < 0)
+    {
+        Console.WriteLine("Factorial cannot be calculated for a negative number!");
+    }
+    else
+    {
+        BigInteger factorial = CalculateFactorial(number);
+        Console.WriteLine($"The factorial of {number} is {factorial}");
+    }
+}
+else
+{
+    Console.WriteLine("Enter a valid number!");
+}
---
+++ task-1/README.md
@@ -0,0 +1,17 @@
+# Basic Data Types, Control Structures, and Methods
+
+## Requirements
+- Write a console application that calculates the factorial of a given number.
+- Read an integer from the user
+- Validate the input (ensure it’s a positive integer).
+- Use loops (or recursion) to calculate the factorial.
+- Display the result in the console.
+
+## To run the program
+```
+dotnet run
+```
+
+## References
+- https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/types/how-to-convert-a-string-to-a-number
+- https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/tokens/interpolated
