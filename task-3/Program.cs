3843afea4ac32ac7bfb816c9ab3f0cc121f4f5ed
SanthoshKannanSP <mail2santhoshkannan@gmail.com>
2025-04-02 23:05:43 +0530

    Task-3 collections and string manipulation

---
+++ task-3/Program.cs
@@ -0,0 +1,95 @@
+﻿class Program
+{
+    public static List<string> Tasks = new List<string>();
+    public static void PrintInstructions()
+    {
+        Console.WriteLine("\n");
+        Console.WriteLine("[V]iew all tasks");
+        Console.WriteLine("[A]dd a new task");
+        Console.WriteLine("[R]emove a task");
+        Console.WriteLine("[E]xit the program");
+    }
+    
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Todo Application!");
+        PrintInstructions();
+        String userInput = Console.ReadLine().ToLower();
+        do
+        {
+            switch (userInput)
+            {
+                case "v":
+                    ViewAllTasks();
+                    break;
+                case "a":
+                    AddTask();
+                    break;
+                case "r":
+                    RemoveTask();
+                    break;
+                default:
+                    Console.WriteLine("Enter a valid input!");
+                    break;
+            }
+            PrintInstructions();
+            userInput = Console.ReadLine().ToLower();
+        } while (userInput != "e");
+    }
+
+    private static void RemoveTask()
+    {
+        Console.WriteLine("\n");
+        if (Tasks.Count == 0)
+        {
+            Console.WriteLine("Nothing to remove!");
+            return;
+        }
+        
+        Console.WriteLine("Enter the task number to be removed: ");
+        if (int.TryParse(Console.ReadLine(), out var taskNumber))
+        {
+            if (taskNumber >0 && taskNumber <= Tasks.Count)
+            {
+                Tasks.RemoveAt(taskNumber-1);
+                Console.WriteLine("Removed Task!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid task number!");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid task number!");
+        }
+    }
+
+    private static void AddTask()
+    {
+        Console.WriteLine("\n");
+        Console.WriteLine("Enter task name: ");
+        var task = Console.ReadLine().Trim();
+        if (task.Length == 0)
+        {
+            Console.WriteLine("Enter a task!!");
+            return;
+        }
+        Tasks.Add(task);
+    }
+
+    private static void ViewAllTasks()
+    {
+        Console.WriteLine("\n");
+        if (Tasks.Count == 0)
+        {
+            Console.WriteLine("No tasks to do!");
+            return;
+        }
+
+        for (var index = 0; index < Tasks.Count; index++)
+        {
+            Console.WriteLine($"[{index+1}] {Tasks[index]}");
+        }
+    }
+}
---
+++ task-3/README.md
@@ -0,0 +1,16 @@
+# Task-3 - Basic Collections and String Manipulation
+
+## Requirements
+- Write a program to manage a list of strings (e.g., names or tasks).
+- Use a List<string> to store items
+- Allow the user to add, remove, and display items
+- Utilize loops and basic string methods (like Trim(), ToUpper()) to process user input
+
+## To run the program
+```
+dotnet run
+```
+
+## References
+- https://learn.microsoft.com/en-us/dotnet/api/system.collections.generic.list-1?view=net-6.0
+- https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/selection-statements#the-switch-statement
