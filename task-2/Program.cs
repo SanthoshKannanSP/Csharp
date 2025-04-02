f5a9dc4556b03055cce02ca23c4dc36c99a238f3
SanthoshKannanSP <mail2santhoshkannan@gmail.com>
2025-04-02 21:46:50 +0530

    Task-2 object-oriented programming

---
+++ task-2/Program.cs
@@ -0,0 +1,30 @@
+﻿class Person
+{
+    private string Name { get; set; }
+    private int Age { get; set; }
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+    
+    public void Introduce()
+    {
+        Console.WriteLine($"Hello, {Name}!");
+    }
+}
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        var ram = new Person("ram",22);
+        var santhosh = new Person("santhosh", 23);
+        var varun = new Person("varun", 24);
+
+        ram.Introduce();
+        santhosh.Introduce();
+        varun.Introduce();
+    }
+}
---
+++ task-2/README.md
@@ -0,0 +1,15 @@
+# Task-2 - Simple Object-Oriented Programming (OOP)
+
+## Requirements:
+- Create a `Person` class with properties and methods.
+- Define properties such as `Name` and `Age`.
+- Implement a method `Introduce()` that prints a personalized greeting.
+- Instantiate a few `Person` objects in your `Main` method and call `Introduce()` on each.
+
+## To run the program
+```csharp
+dotnet run
+```
+
+## References
+- https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/object-oriented/objects
