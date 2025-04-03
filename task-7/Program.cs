fd5b00adb9987293271bfd3140bb0d4ed76308f3
SanthoshKannanSP <mail2santhoshkannan@gmail.com>
2025-04-03 22:38:45 +0530

    Task-7 asynchronous programming

---
+++ task-7/Program.cs
@@ -0,0 +1,34 @@
+﻿class Program
+{
+    private static Random _randomGenerator = new Random();
+    
+    public static async Task Main(string[] args)
+    {
+        
+        var appleStockPrice = FetchStockPrice("AAPL");
+        var microsoftStockPrice =  FetchStockPrice("MSFT");
+        var nvidiaStockPrice =  FetchStockPrice("NVDA");
+        int[] stockPrices;
+        try
+        {
+            stockPrices = await Task.WhenAll(appleStockPrice, microsoftStockPrice, nvidiaStockPrice);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error Fetching the Data!");
+            return;
+        }
+
+        var totalPrice = stockPrices.Sum();
+        
+        Console.WriteLine($"Total stock price is {totalPrice}");
+    }
+
+    private static async Task<int> FetchStockPrice(string stockCode)
+    {
+        var waitTime = _randomGenerator.Next(1, 5) * 1000;
+        await Task.Delay(waitTime);
+        var stockPrice = _randomGenerator.Next(50, 100);
+        return stockPrice;
+    }
+}
---
+++ task-7/README.md
@@ -0,0 +1,11 @@
+# Task-7 - Asynchronous Programming and Multi-threading
+
+## Requirements
+- Develop a console application that performs multiple asynchronous operations concurrently.
+- Use async and await to fetch data from multiple simulated sources (e.g., using Task.Delay to mimic API calls).
+- Aggregate the results once all tasks are complete.
+- Handle exceptions that may occur during asynchronous operations.
+
+## References
+- https://learn.microsoft.com/en-us/dotnet/api/system.threading.tasks.task.delay?view=net-6.0
+- https://learn.microsoft.com/en-us/dotnet/csharp/asynchronous-programming/
